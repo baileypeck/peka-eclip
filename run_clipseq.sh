5943e97fc0db2591de817fb58dde1c30d17d57c6
baileypeck <150298139+baileypeck@users.noreply.github.com>
2023-12-14 21:22:19 -0600

    Update run_clipseq.sh

--- run_clipseq.sh
+++ run_clipseq.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
+# Script name = nextflowclipseq
+echo "nextflowclipseq"
+echo "##################"
+echo "Created by = bdpeck"
+echo "##"
+echo "Date Created = 12DEC23 "
+echo "##"
+echo "Date Updated = 12DEC23"
+echo "##"
+echo "Date Job Run = 13DEC23"
+echo "#################"
+#$ -N nextflowclipseq
+#$ -q CCOM
+#$ -M bdpeck@uiowa.edu
+#$ -m beas
+#$ -j Y
+#$ -o /Users/bdpeck/qsubout.txt
+#$ -pe smp 16
+#$ -l h_vmem=1000G
+#$ -l h_rt=1000:0:0
+#################
 
-ml purge
-ml Nextflow/21.04.0
-ml Singularity/3.6.4
-ml Graphviz/2.38.0-foss-2016b
-
-export NXF_SINGULARITY_CACHEDIR=/camp/lab/luscomben/home/shared/singularity
-REFDIR=/camp/lab/luscomben/home/users/chakraa2/projects/peka/eclip/ref
-
-## UPDATE PIPELINE
-nextflow pull amchakra/clipseq -r dev
-
-## RUN PIPELINE
-nextflow run amchakra/clipseq -r dev \
--profile crick \
---input samplesheet.csv \
---outdir results_clipseq \
---umi_separator _ \
---smrna_org human \
---fasta $REFDIR/GRCh38.primary_assembly.genome.fa.gz \
---gtf $REFDIR/gencode.v30.primary_assembly.annotation.gtf.gz \
---star_index $REFDIR/STAR_GRCh38_primary_gencode_v30
+REFDIR=/Users/bdpeck/ref
+cd /Users/bdpeck
+module load  stack/2022.2
+module load stack/2022.2-base_arch
+module load nextflow/22.10.1_gcc-9.5.0
+nextflow pull baileypeck/peka-eclip
+nextflow run baileypeck/peka-eclip
+-profile 'singularity' \
+--input 'sample.csv' \
+--outdir 'results' \
+--umi_separator '_' \
+--smrna_org 'human' \
+--fasta $REFDIR/gencode.v44.transcripts.fa \
+--gtf $REFDIR/gencode.v44.basic.annotation.gtf \
